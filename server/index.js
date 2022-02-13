61792ab4581473d1d1dd84c4cea1f18623aa3525
halilibrahimdemircan <58706397+halilibrahimdemircan@users.noreply.github.com>
2022-02-13 08:27:40 +0300

    models and routes

--- server/index.js
+++ server/index.js
@@ -3,9 +3,10 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import mongoose from 'mongoose';
-
-
+import dotenv from "dotenv"
+import postRoutes from './routes/posts';
 const app = express();
+dotenv.config();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -14,10 +15,12 @@ app.use(cors());
 app.get("/", (req,res) =>{
     res.send("Hello from Blogify App Backend")
 })
+app.use("/posts", postRoutes)
 
 const PORT = process.env.PORT || 8000;
-
-mongoose.connect(process.env.mongoURI, { useNewUrlParser: true }).then(() => {
+const URL = "mongodb+srv://halilibrahimdemircan:Halil3677@cluster0.9njpd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+ 
+mongoose.connect(URL, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
     app.listen(PORT, () => {
         console.log(`Server is running on port ${PORT}`)
 })})
