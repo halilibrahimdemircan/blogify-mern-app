22a4ee650d1ff4f026a4929c13cf56fded945db7
halilibrahimdemircan <58706397+halilibrahimdemircan@users.noreply.github.com>
2022-03-16 00:21:21 +0300

    addpostform

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { makeStyles } from '@material-ui/core'
-import {CssBaseline, Container, Grid, AppBar, Toolbar, Typography, Button, IconButton} from "@material-ui/core"
+import { CssBaseline, Container, Grid, AppBar, Toolbar, Typography, Button, IconButton } from "@material-ui/core"
 import PenIcon from "@material-ui/icons/Create";
-import {BrowserRouter as Router, Route, Navigate, Routes} from "react-router-dom"
+import { BrowserRouter as Router, Route, Navigate, Routes } from "react-router-dom"
 import PostList from './components/PostList';
+import AddPostForm from './components/AddPostForm';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -22,33 +23,52 @@ const useStyles = makeStyles(theme => ({
 
 const App = () => {
 
+  const [open, setOpen] = useState(false)
+
+
+  const handleOpen = () => {
+    setOpen(true)
+  }
+
+  const handleClose = () => {
+    setOpen(false)
+  }
+
+
+
+
+
+
   const classes = useStyles()
   return (
     <>
-    <CssBaseline/>
-    <Container maxWidth="lg">
-      <AppBar position='static' color='inherit' elevation={0}>
-        <Toolbar>
-          <IconButton edge="start" className={classes.container} color="inherit" />
-          <Typography variant='h6' color='secondary' className={classes.title}>
-            <a href='http://localhost:3000/posts'>Blogify</a>
-          </Typography>
-          <Button color='primary' variant='outlined' startIcon={<PenIcon/>}>Yeni Yazı</Button>
-        </Toolbar>
-      </AppBar>
-      <Grid container className={classes.container} >
-        <Grid item xs={12}>
-          <Router>
-            <Routes>
-              <Route exact path='/posts' component={PostList}/>
-            </Routes>
-            <Navigate from="/" to="/posts" />
-          </Router>
+      <CssBaseline />
+      <Container maxWidth="lg">
+        <AppBar position='static' color='inherit' elevation={0}>
+          <Toolbar>
+            <IconButton edge="start" className={classes.container} color="inherit" />
+            <Typography variant='h6' color='secondary' className={classes.title}>
+              <a href='http://localhost:3000/posts'>Blogify</a>
+            </Typography>
+            <Button color='primary' variant='outlined' startIcon={<PenIcon />}>Yeni Yazı</Button>
+          </Toolbar>
+        </AppBar>
+        <Grid container className={classes.container} >
+          <Grid item xs={12}>
+            <Router>
+              <Routes>
+                <Route exact path='/posts' component={PostList} />
+              </Routes>
+              <Navigate from="/" to="/posts" />
+            </Router>
+          </Grid>
         </Grid>
-      </Grid>
-    </Container>
-    
-    
+      </Container>
+
+      <AddPostForm open={open} handleClose={handleClose} />
+
+
+
     </>
   )
 }
---
+++ client/src/components/AddPostForm.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { makeStyles } from '@material-ui/core'
+import { Button, TextField, Select, Input, MenuItem, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle } from "@material-ui/core"
+import { useForm, Controller } from "react-hook-form"
+import * as yup from "yup"
+import { yupResolver } from "hookform/resolvers"
+
+
+
+const AddPostForm = () => {
+
+
+    return (
+        <div>AddPostForm</div>
+    )
+}
+
+export default AddPostForm
